872400d9619f91398f8233eefdecaae8cf06fc35
kaze892 <47201743+kaze892@users.noreply.github.com>
2021-06-16 23:22:11 +0900

    Add files via upload

---
+++ asset/caculator.js
@@ -0,0 +1,397 @@
+
+$(document).ready(function(){
+	var0 = 0;
+	var100 = 0;
+	var200 = 0;
+	var300 = 0;
+	var400 = 0;
+	var500 = 0;
+	var600 = 0;
+	var700 = 0;
+	var800 = 0;
+	var900 = 0;
+	varsam =0;
+	f100= 0;
+	f200= 0 ;
+	f300= 0 ;
+	f400= 0 ;
+	f500= 0 ;
+	f600= 0 ;
+	f700= 0 ;
+	f800= 0 ;
+	f900= 0 ;
+	
+	
+$('#checkbox1_1').click(function () {
+	if(f100 == 0){
+	$('#checkbox1_1').css({'border-color':'#1d9e9d','opacity':'0.4'});
+	
+		var100 =100;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		
+		$("p.sam").text(varsam);
+		f100 = 1;
+	}else{
+		$('#checkbox1_1').css({'border-color':'#fff','opacity':'1'});
+		var100 =  0;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		
+		$("p.sam").text(varsam);
+		f100 = 0;
+	}
+});
+$('#checkbox1_2').click(function () {
+	$('#checkbox1_2').css({'border-color':'#1d9e9d','opacity':'0.4'});
+	if(f200 == 0){
+		var200 =200;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f200 = 1;
+	}else{
+		$('#checkbox1_2').css({'border-color':'#fff','opacity':'1'});
+		var200 =  0;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f200 = 0;
+	}
+});
+$('#checkbox1_3').click(function () {	
+	$('#checkbox1_3').css({'border-color':'#1d9e9d','opacity':'0.4'});
+	if(f300 == 0){
+		var300 =300;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f300 = 1;
+	}else{
+		$('#checkbox1_3').css({'border-color':'#fff','opacity':'1'});
+		var300 =  0;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f300 = 0;
+	}
+});
+$('#checkbox1_4').click(function () {
+	$('#checkbox1_4').css({'border-color':'#1d9e9d','opacity':'0.4'});
+	if(f400 == 0){
+		var400 =400;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f400 = 1;
+	}else{
+		$('#checkbox1_4').css({'border-color':'#fff','opacity':'1'});
+		var400 =  0;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f400 = 0;
+	}
+});
+$('#checkbox1_5').click(function () {
+	$('#checkbox1_5').css({'border-color':'#1d9e9d','opacity':'0.4'});
+	if(f500 == 0){
+		var500 =500;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f500 = 1;
+	}else{
+		$('#checkbox1_5').css({'border-color':'#fff','opacity':'1'});
+		var500 =  0;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		$("p.sam").text(varsam);
+		f500 = 0;
+	}
+});
+$('#checkbox1_6').click(function () {
+	$('#checkbox1_6').css({'border-color':'#1d9e9d','opacity':'0.4'});
+	if(f600 == 0){
+		var600 =600;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f600 = 1;
+	}else{
+		$('#checkbox1_6').css({'border-color':'#fff','opacity':'1'});
+		var600 =  0;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f600 = 0;
+	}
+});
+$('#checkbox1_7').click(function () {
+	$('#checkbox1_7').css({'border-color':'#1d9e9d','opacity':'0.4'});
+	if(f700 == 0){
+		var700 =700;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f700 = 1;
+	}else{
+		$('#checkbox1_7').css({'border-color':'#fff','opacity':'1'});
+		var700 =  0;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f700 = 0;
+	}
+});
+$('#checkbox1_8').click(function () {
+	$('#checkbox1_8').css({'border-color':'#1d9e9d','opacity':'0.4'});
+	if(f800 == 0){
+		var800 =800;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f800 = 1;
+	}else{
+		$('#checkbox1_8').css({'border-color':'#fff','opacity':'1'});
+		var800 =  0;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f800 = 0;
+	}
+});
+$('#checkbox1_9').click(function () {
+	$('#checkbox1_9').css({'border-color':'#1d9e9d','opacity':'0.4'});
+	if(f900 == 0){
+		var900 =900;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f900 = 1;
+	}else{
+		$('#checkbox1_9').css({'border-color':'#fff','opacity':'1'});
+		var900 =  0;
+		varsam = var0  + var100 + var200 + var300 + var400 + var500 + var600 +var700 + var800 + var900;
+		if(varsam ==2000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==3000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4000){
+			$('p.sam').css({'color':'red',});
+		}else if(varsam ==4500){
+		$('p.sam').css({'color':'red',});
+		}else {
+		$('p.sam').css({'color':'black',});
+		}
+		$("p.sam").text(varsam);
+		f900 = 0;
+	}
+});
+$('#reset').click(function () {
+	 // location.reload();
+	$('#checkbox1_1').prop('checked',true);
+	
+	if(f100 == 1){
+	$("#checkbox1_1").trigger("click");
+	}
+	if(f200 == 1){
+	$("#checkbox1_2").trigger("click");
+	}
+	if(f300 == 1){
+	$("#checkbox1_3").trigger("click");
+	}
+	if(f400 == 1){
+	$("#checkbox1_4").trigger("click");
+	}
+	if(f500 == 1){
+	$("#checkbox1_5").trigger("click");
+	}
+	if(f600 == 1){
+	$("#checkbox1_6").trigger("click");
+	}
+	if(f700 == 1){
+	$("#checkbox1_7").trigger("click");
+	}
+	if(f800 == 1){
+	$("#checkbox1_8").trigger("click");
+	}
+	if(f900 == 1){
+	$("#checkbox1_9").trigger("click");
+	}
+	
+	varsam = 0;
+	  f100 = 0;
+	  f200 = 0;
+	  f300 = 0;
+	  f400 = 0;
+	  f500 = 0;
+	  f600 = 0;
+	  f700 = 0;
+	  f800 = 0;
+	  f900 = 0;
+ $("p.sam").text(varsam);
+	  
+	   
+    });
+  });
